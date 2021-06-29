472372250bdd6c6b79b4de3b909470b2c8402d8f
hophoet <hophoet@gmail.com>
2021-06-29 23:09:28 +0000

    (data) - add presentation test data

---
+++ src/layouts/presentation/data.js
@@ -0,0 +1,28 @@
+
+export default [
+	{
+		id:'1',
+		title:'Welcome on CashChap',
+		description:'Grow your bisiness by acception card payments with the new card reader.',
+		image: require('../../assets/images/img1.png'),
+	},
+	{
+		id:'2',
+		title:'Smart Point of Sale',
+		description:'Grow your bisiness by acception card payments with the new card reader.',
+		image: require('../../assets/images/img2.png'),
+	},
+	{
+		id:'3',
+		title:'Instant Notification',
+		description:'Grow your bisiness by acception card payments with the new card reader.',
+		image: require('../../assets/images/img3.png'),
+	},
+	{
+		id:'4',
+		title:'Customize Everything',
+		description:'Grow your bisiness by acception card payments with the new card reader.',
+		image: require('../../assets/images/img2.png'),
+		
+	},
+]
