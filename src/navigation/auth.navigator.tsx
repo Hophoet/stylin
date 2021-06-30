25c1846db87f6f24dd60fc32364df0328d06738d
hophoet <hophoet@gmail.com>
2021-06-30 21:11:04 +0000

    (navigation) - add CodeVerification screen component to auth navigator

--- src/navigation/auth.navigator.tsx
+++ src/navigation/auth.navigator.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import Presentation from '../layouts/presentation/Presentation';
 import Register from '../layouts/auth/Register';
+import Test from '../layouts/auth/Test';
+import CodeVerification from '../layouts/auth/CodeVerification';
 
 const Stack = createStackNavigator();
 
@@ -9,5 +11,6 @@ export const AuthNavigator = (): React.ReactElement => (
   <Stack.Navigator headerMode='none'>
     <Stack.Screen name='Presentation' component={Presentation}/>
     <Stack.Screen name='Register' component={Register}/>
+    <Stack.Screen name='CodeVerification' component={CodeVerification}/>
   </Stack.Navigator>
 );
