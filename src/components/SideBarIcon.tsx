bb6a130f11e0fc1c6a16e96daa04611253e437aa
hophoet <hophoet@gmail.com>
2021-07-01 04:38:53 +0000

    (component) - update SideBarIcon component

--- src/components/SideBarIcon.tsx
+++ src/components/SideBarIcon.tsx
@@ -1,28 +1,69 @@
 import React, {useEffect, useState} from 'react';
 import {TextInput, Image, ScrollView, FlatList, Text, View, StyleSheet, TouchableOpacity, Dimensions} from 'react-native'
 import Icon from "react-native-vector-icons/Ionicons";
+import { bgLinearGradient, sideBarLinearGradient} from '../assets/colors/main';
 
 
 
-const SideBar = ({item}:any) => {
-
-	console.log(item)
+const SideBarIcon = ({item, activeMenu, setActiveMenu}:any) => {
+	const isActiveMenu = () =>{
+		return item == activeMenu;
+		
+	}
 	return(
-		<TouchableOpacity style={styles.container}>
-			<Icon name={item.icon} size={30} color='gray' />
-		</TouchableOpacity>
+		<View 
+			style={(isActiveMenu())
+				?styles.activeContainer
+				:styles.container}>
+			<TouchableOpacity 
+				onPress={()=>{
+					setActiveMenu(item)
+				}}
+				style={(isActiveMenu())
+					?styles.activateButtonContainer
+					:styles.buttonContainer}>
+				<Icon 
+					name={item.icon} 
+					size={30} 
+					color={(isActiveMenu())
+						?sideBarLinearGradient.top
+						:'black' 
+					}
+				/>
+			</TouchableOpacity>
+		</View>
 	)
 }
-export default SideBar;
+export default SideBarIcon;
 
 const {width, height} = Dimensions.get('window');
 const styles = StyleSheet.create({
 	container:{
+		padding:10,
+		borderRadius:width/2,
+	},
+	activeContainer:{
+		padding:10,
+		borderRadius:width/2,
+
+	},
+	buttonContainer:{
 		backgroundColor:'white',
 		borderRadius:width/2,
 		width:width/8,
 		height:width/8,
 		justifyContent:'center',
 		alignItems:'center',
+		elevation:10,
 	},
+	activateButtonContainer:{
+		backgroundColor:'white',
+		borderRadius:width/2,
+		width:width/7,
+		height:width/7,
+		justifyContent:'center',
+		alignItems:'center',
+		elevation:10,
+
+	}
 })
