2a31077b53cbcd2268511d902c22cef8d644c207
hophoet <hophoet@gmail.com>
2021-07-01 04:35:09 +0000

    (colors) - add sideBar linear gradient colors

--- src/assets/colors/main.ts
+++ src/assets/colors/main.ts
@@ -1,9 +1,16 @@
 
 const bgLinearGradient = {
     top:'#53556E',
-    bottom:'#303441',
+    center:'#303441',
+    bottom:'black'
+    // bottom:'#303441',
 }
+const sideBarLinearGradient = {
+    top:'#33DDFF', 
+    bottom:'#1FA9FF'
+} 
 
 export {
-    bgLinearGradient
+    bgLinearGradient,
+    sideBarLinearGradient,
 }
