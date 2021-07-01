86dedfcc0123be5a6ab036e7065fb4cb07920eff
hophoet <hophoet@gmail.com>
2021-07-01 04:39:26 +0000

    (layout) - update Main layout component

--- src/layouts/main/Main.tsx
+++ src/layouts/main/Main.tsx
@@ -6,30 +6,72 @@ import {
   View,
   StatusBar,
   TouchableOpacity,
+  ScrollView,
 } from "react-native";
 import LinearGradient from 'react-native-linear-gradient';
 import PhoneInput from "react-native-phone-number-input";
+import Icon from "react-native-vector-icons/Ionicons";
 import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
 
 import { bgLinearGradient} from '../../assets/colors/main';
 import SideBar from '../../components/SideBar';
+import Home from './Home';
+
+const icons = [
+    {id:1, icon:'home'},
+    {id:2, icon:'settings'},
+    {id:3, icon:'heart'},
+    {id:4, icon:'card'},
+    {id:5, icon:'scan'},
+]
 
 
 const Main = ({navigation}:any) => {
-
+  const [activeMenu, setActiveMenu] = useState(icons[0]);
   const [value, setValue] = useState("");
+  const [sideBarVisible, toggleSideBar] = useState(false);
+
   return (
       <View style={styles.container}>
         <StatusBar backgroundColor={bgLinearGradient.top} barStyle="dark-content" />
-		<View style={styles.sideBarContainer}>
-        	<SideBar/>
-		</View>
+		{ sideBarVisible && 
+			<View style={[styles.sideBarContainer]}>
+				<SideBar
+					activeMenu={activeMenu}
+					setActiveMenu={setActiveMenu}
+					menus={icons}
+					sideBarVisible={sideBarVisible}	
+					toggleSideBar={toggleSideBar}	
+				/>
+			</View>
+		}
 		<View style={styles.contentContainer}>
 			  <LinearGradient 
-          		colors={[bgLinearGradient.top, bgLinearGradient.bottom]} 
+          		colors={[bgLinearGradient.top, bgLinearGradient.center]} 
           		style={styles.linearGradient}>
+					  <View style={styles.header}>
+						  	<View style={[styles.headerColumn, styles.headerColumn1]}>
+							{ !sideBarVisible && 
+								<TouchableOpacity
+									onPress={()=>toggleSideBar(!sideBarVisible)}
+								>
+									<Icon name='list' size={30} color='white'/>
+								</TouchableOpacity>
+							}
+						  </View>
+						  <View style={[styles.headerColumn, styles.headerColumn2]}>
+							  <Text style={styles.title}>cashchap</Text>
+						  </View>
+					  </View>
+					  <View 
+					  	style={styles.screenContainer}
+						>
+							<ScrollView>
+								<Home/>
+							</ScrollView>
+							  
+						</View>
 			
-            	<Text>APP MAIN SCREEN</Text>
 			</LinearGradient>
 		</View>
       </View>
@@ -59,5 +101,33 @@ const styles = StyleSheet.create({
   	linearGradient:{
 		  flex:1,
 
-  },
+  	},
+	title:{
+		color:'white',
+		fontSize:20,
+	},
+  	header:{
+		flexDirection:'row',
+		height:width/8,
+		// backgroundColor:'red',
+
+  	},
+	headerColumn:{
+		flex:1,
+		justifyContent:'center',
+	},
+	headerColumn1:{
+		alignItems:'flex-start',
+		paddingLeft:width/12,
+	},
+	headerColumn2:{
+		alignItems:'flex-end',
+		paddingRight:width/12,
+	},
+	screenContainer:{
+		flex:1,
+		paddingLeft:width/12,
+	}
+
+
 })
