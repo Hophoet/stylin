4893073fbcfe027160ddf11ce8721532af37f5c8
hophoet <hophoet@gmail.com>
2021-07-02 12:05:14 +0000

    (navigation) add Transfer screen component

--- src/navigation/main.navigator.tsx
+++ src/navigation/main.navigator.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import Main from '../layouts/main/Main'
+import Transfer from '../layouts/main/Transfer';
 
 const Stack = createStackNavigator();
 
 export const MainNavigator = (): React.ReactElement => (
   <Stack.Navigator headerMode='none'>
     <Stack.Screen name='Main' component={Main}/>
+    <Stack.Screen name='Transfer' component={Transfer}/>
   </Stack.Navigator>
 );
