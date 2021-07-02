8328e47916686e6f2dd0979226b0a3e945725a58
hophoet <hophoet@gmail.com>
2021-07-02 14:18:28 +0000

    (navigation) update main navigator

--- src/navigation/navigator.tsx
+++ src/navigation/navigator.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
-import {AuthNavigator} from './auth.navigator'
-import {MainNavigator} from './main.navigator'
+import {AuthNavigator} from './auth.navigator';
+import {MainNavigator} from './main.navigator';
 
 const Stack = createStackNavigator();
 
 export const Nav = (): React.ReactElement => (
   <Stack.Navigator headerMode='none'>
-    <Stack.Screen name='Auth' component={AuthNavigator}/>
     <Stack.Screen name='Main' component={MainNavigator}/>
+    <Stack.Screen name='Auth' component={AuthNavigator}/>
   </Stack.Navigator>
 );
 
