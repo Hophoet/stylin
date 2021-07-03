01ffe31887d868176b180e8e29beb53d885963c0
hophoet <hophoet@gmail.com>
2021-07-03 02:17:03 +0000

    (component) add action button component

---
+++ src/components/ActionButton.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {Text, StyleSheet, TouchableOpacity, Dimensions} from 'react-native'
+import Icon from "react-native-vector-icons/Ionicons";
+
+
+const ActionButton = ({item, onPress, iconPosition, buttonStyle, icon, textStyle, iconColor}:any) => {
+	return(
+		<TouchableOpacity 
+            onPress={onPress}
+            style={[styles.container, buttonStyle]}>
+            <Text style={[styles.buttonLabel, textStyle]}>{item.name}</Text>
+            { icon &&
+                <Icon name={icon} size={30} color={(iconColor)?iconColor:'black'} />
+            }
+		</TouchableOpacity>
+	)
+}
+export default ActionButton;
+
+const {width, height} = Dimensions.get('window');
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor:'white',
+		flexDirection:'row',
+		justifyContent:'space-between',
+		padding:10,
+		borderRadius:5,
+		margin:5,
+		minHeight:50,
+		elevation:5,
+	},
+	buttonLabel:{
+		fontWeight:'bold',
+        color:'black',
+		margin:5,
+		fontSize:17,
+	}
+
+})
