270f9657aa4d5e21a0d20666dc962ca76c6fc092
hophoet <hophoet@gmail.com>
2021-07-03 06:45:44 +0000

    (data) - add test default data

---
+++ src/data/data.ts
@@ -0,0 +1,24 @@
+const productCategories = [
+    { id:1, name:'sport shoe'},
+    { id:2, name:'shoe'},
+    { id:5, name:'t shirt'},
+    { id:4, name:'men shoe'},
+    { id:7, name:'women shoe'},
+    { id:8, name:'kid shoe'},
+    { id:9, name:'clothes'},
+    { id:10, name:'baby clothes'},
+    { id:11, name:'men clothes'},
+    { id:12, name:'women clothes'},
+]
+
+const products = [
+    {id:1, name:'Black Shirt', caracteristics:'kjdf ldfjlsdfjsldfjs flsdfjlsfjsldfjskldfs dfslfjksldf sdfk', price:49, isFavorite:true, image:require('../assets/images/bg1.jpg')},
+    {id:2, name:'Black Shirt', caracteristics:'kjdf ldfjlsdfjsldfjs flsdfjlsfjsldfjskldfs dfslfjksldf sdfk', price:49, isFavorite:true, image:require('../assets/images/bg4.jpg')},
+    {id:3, name:'Black Shirt', caracteristics:'kjdf ldfjlsdfjsldfjs flsdfjlsfjsldfjskldfs dfslfjksldf sdfk', price:49, isFavorite:true, image:require('../assets/images/img4.jpg')},
+    {id:4, name:'Black Shirt', caracteristics:'kjdf ldfjlsdfjsldfjs flsdfjlsfjsldfjskldfs dfslfjksldf sdfk', price:49, isFavorite:true, image:require('../assets/images/img5.jpg')},
+]
+
+export {
+    productCategories,
+    products
+}
