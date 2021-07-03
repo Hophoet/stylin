e40631ce1ae634f7f586aba2fbd9c575f2637bc8
hophoet <hophoet@gmail.com>
2021-07-03 06:51:14 +0000

    (component) update home screen set custom header component set the header slider component render the product category & handle their selection render the product on 2 columns

--- src/layouts/main/Home.tsx
+++ src/layouts/main/Home.tsx
@@ -7,14 +7,15 @@ import {
   Text,
   StatusBar,
   TouchableOpacity,
+  ImageBackground,
+  ScrollView,
+  FlatList
 } from "react-native";
-import LinearGradient from 'react-native-linear-gradient';
-import PhoneInput from "react-native-phone-number-input";
-import Svg, { Circle, Text as CsvText } from "react-native-svg";
-import { ApplicationProvider, Layout } from '@ui-kitten/components';
-
 import { bgLinearGradient, sideBarLinearGradient} from '../../assets/colors/main';
-import IconActionBox from '../../components/IconActionBox';
+import HomeHeader from '../../components/HomeHeader';
+import ProductItem from '../../components/ProductItem';
+
+import {productCategories, products} from '../../data/data'
 
 const iconActions = [
 	{icon:'wifi', action:()=>{}},
@@ -22,72 +23,74 @@ const iconActions = [
 ]
 
 const Home = ({navigation}:any) => {
-
   const [value, setValue] = useState("");
+  const [selectedCategory, selectCategory] = useState(productCategories[0]);
+
   return (
       <View style={styles.container}>
-		<View style={styles.row1}>
-            	<Text style={styles.title}>Bienvenue</Text>
-            	<Text style={styles.description}>
-					Lorem aut, consectetur blanditiis enim eligendi amet voluptates rem tempora qui a repellat vitae quia illum adipisci obcaecati architecto. Ullam, ducimus excepturi!	Bienvenue
-				</Text>
-		</View>
-		<View style={styles.row2}>
-			<Svg width={width/2} height={width/2}>
-					<CsvText
-					// stroke="white"
-					fontSize="25"
-					x={width / 4}
-					y={width / 4}
-					fill='white'
-					textAnchor="middle"
-					>
-					CashChap
-					</CsvText>
-					<CsvText
-					// stroke="white"
-					fontSize="12"
-					x={width / 4}
-					y={width / 3.3}
-					fill='white'
-					textAnchor="middle"
-					>
-						l'echange devient simple
-					</CsvText>
-				 <Circle
-					cx={width/4}
-					cy={width/4}
-					r={width/5}
-					stroke={sideBarLinearGradient.top}
-					strokeWidth="2.5"
-					fill='none'
-				  />
-				{/* <Circle
-				  stroke="#2162cc"
-				  fill="none"
-				  cx={size / 2}
-				  cy={size / 2}
-				  r={radius}
-				  strokeDasharray={`${circunference} ${circunference}`}
-				/> */}
-			  </Svg>
-		</View>
-		<View style={styles.row3}>
-            	<Text style={styles.row3Title}>
-					Lorem ipsum  aut, consectetur lenz ldfk df
-					Lorem ipsum  aut, consectetur lené l
-				</Text>
-		</View>
-		<View style={styles.row4}>
-			<View style={styles.row4Row}>
-				<IconActionBox icon='settings' title='Parametrer'/>
-				<IconActionBox icon='send' title='Envoyer'/>
-			</View>
-			<View style={styles.row4Row}>
-				<IconActionBox icon='receipt' title='Activer'/>
-				<IconActionBox icon='card' title='Recevoir'/>
-			</View>
-		</View>
+        <HomeHeader/>
+        <View style={styles.row1}>
+          <ImageBackground 
+            source={require('../../assets/images/hbg.jpg')}
+            style={styles.row1ImageBackground}>
+              <View style={styles.row1ImageBackgroundContentContainer}>
+                  <Text style={styles.row1Label1}>30% off</Text>
+                  <Text style={styles.row1Label2}>Bonus on order</Text>
+              </View>
+
+            </ImageBackground>
+        </View>
+        <View style={styles.row2}>
+          <ScrollView 
+            horizontal 
+            showsHorizontalScrollIndicator={false}
+          >
+
+          {
+            productCategories.map((item, index) =>  (
+                  <TouchableOpacity 
+                    onPress={()=>selectCategory(item)}
+                    style={styles.categoryButton}
+                    key={index.toString()}>
+                    <Text style={[styles.categoryName,
+                      (selectedCategory == item) && {textDecorationLine:'underline'}
+                    
+                    ]}>{item.name}</Text>
+                  </TouchableOpacity>
+            )
+            )
+          }
+          </ScrollView>
+        </View>
+        <View style={styles.row3}>
+          <View style={styles.row3Column1}>
+              <FlatList
+                data={products}
+                showsVerticalScrollIndicator={false}
+                keyExtractor={(item) =>item.id.toString()}
+                renderItem={({item, index})=> (
+                  <ProductItem 
+                    width={width/2}
+                    height={width/2}
+                    item={item}/>
+                )}
+              />
+          </View>
+          <View style={styles.row3Column2}>
+
+              <FlatList
+                data={products}
+                showsVerticalScrollIndicator={false}
+                keyExtractor={(item) =>item.id.toString()}
+                renderItem={({item, index})=> (
+                  <ProductItem 
+                    width={width/2}
+                    height={width/1}
+                    item={item}/>
+                )}
+              />
+          </View>
+        </View>
       </View>
   );
 
@@ -102,38 +105,55 @@ const styles = StyleSheet.create({
 		flex:1,
 
 	},
-	contentContainer:{
-	},
+  row1:{
+    height:width/2.5,
+  },
+  row2:{
+    flexDirection:'row',
+  },
+  row1ImageBackground:{
+    flex:1, 
+    justifyContent:'center',
+  },
+  row1ImageBackgroundContentContainer:{
+    // backgroundColor:'red',
+    alignSelf:'flex-start',
+    paddingHorizontal:10,
+  },
+  row1Label1:{
+    color:'white',
+    fontWeight:'bold',
+    fontSize:40,
+  },
+  row1Label2:{
+    color:'white',
+    fontSize:20,
+  },
 	title:{
-		color:sideBarLinearGradient.top,
+		color:'black',
 		fontSize:25,
 	},
-	description:{
-		paddingVertical:5,
-		paddingRight:20,
-		color:'white',
-		opacity:.5,
-	},
-	row1:{
+  categoryButton:{
+    paddingHorizontal:10,
+    // backgroundColor:'red',
+    marginHorizontal:5,
+  },
+  categoryName:{
+    fontSize:20,
+    fontWeight:'bold',
+  },
+  row3:{
+    // backgroundColor:'green',
+    flexDirection:'row',
+    flex:1,
+  },
+  row3Column1:{
+    flex:1,
+    // backgroundColor:'red',
 
-	},
-	row2:{
-		// backgroundColor:'red',
-		justifyContent:'center',
-		alignItems:'center',
-	},
-	row3:{
-		// paddingHorizontal:width/8,
-	},
-	row3Title:{
-		textAlign:'center',	
-		paddingVertical:5,
-		paddingRight:20,
-		color:'white',
-		opacity:.5,
-	},
-	row4Row:{
-		flexDirection:'row',
-		justifyContent:'center'
-	}
+  },
+  row3Column2:{
+    flex:1,
+    // backgroundColor:'aqua',
+  }
 })
