87946a0f5cfb4526acd21bf220ecd11e633b1b04
hophoet <hophoet@gmail.com>
2021-07-03 11:52:46 +0000

    (data) - add product sizes, product colors data

--- src/data/data.ts
+++ src/data/data.ts
@@ -11,14 +11,31 @@ const productCategories = [
     { id:12, name:'women clothes'},
 ]
 
+const productColors = [
+    { id:1, color:'red'},
+    { id:2, color:'blue'},
+    { id:5, color:'yellow'},
+]
+
+const productSizes = [
+    { id:1, size:'L'},
+    { id:2, size:'M'},
+    { id:5, size:'X'},
+]
+
 const products = [
-    {id:1, name:'Black Shirt', caracteristics:'kjdf ldfjlsdfjsldfjs flsdfjlsfjsldfjskldfs dfslfjksldf sdfk', price:49, isFavorite:true, image:require('../assets/images/bg1.jpg')},
+    {id:1, name:'Black Shirt', caracteristics:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis facilis qui nobis provident accusamus officia aspernatur natus dicta, perferendis temporibus rem nesciunt odit! Sit dolores mollitia libero? Dolores, assumenda explicabo?', price:49, isFavorite:true, image:require('../assets/images/bg1.jpg')},
     {id:2, name:'Black Shirt', caracteristics:'kjdf ldfjlsdfjsldfjs flsdfjlsfjsldfjskldfs dfslfjksldf sdfk', price:49, isFavorite:true, image:require('../assets/images/bg4.jpg')},
     {id:3, name:'Black Shirt', caracteristics:'kjdf ldfjlsdfjsldfjs flsdfjlsfjsldfjskldfs dfslfjksldf sdfk', price:49, isFavorite:true, image:require('../assets/images/img4.jpg')},
     {id:4, name:'Black Shirt', caracteristics:'kjdf ldfjlsdfjsldfjs flsdfjlsfjsldfjskldfs dfslfjksldf sdfk', price:49, isFavorite:true, image:require('../assets/images/img5.jpg')},
 ]
 
+
+
+
 export {
     productCategories,
-    products
+    products,
+    productColors,
+    productSizes,
 }
