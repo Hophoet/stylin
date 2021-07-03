aacb47bb286a72dba4ffda78d8316b6713c96997
hophoet <hophoet@gmail.com>
2021-07-03 11:49:37 +0000

    (component) add custom icon button component

---
+++ src/components/CIconButton.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {Text, StyleSheet, TouchableOpacity, Dimensions} from 'react-native'
+import Icon from "react-native-vector-icons/Ionicons";
+
+
+const CIconButton = ({label, onPress, iconSize, buttonSize, buttonStyle, icon, textStyle, iconColor}:any) => {
+	return(
+		<TouchableOpacity 
+            onPress={onPress}
+            style={[styles.container, buttonStyle]}>
+				{ icon &&
+            		<Icon name={icon} size={(iconSize)?iconSize:30} color={(iconColor)?iconColor:'black'} />
+				}
+		</TouchableOpacity>
+	)
+
+}
+export default CIconButton;
+
+const {width, height} = Dimensions.get('window');
+const styles = StyleSheet.create({
+    container:{
+		backgroundColor:'white',
+		borderRadius:width/2,
+		justifyContent:'center',
+		alignItems:'center',
+	},
+	buttonLabel:{
+		fontWeight:'bold',
+        color:'black',
+		margin:5,
+		fontSize:17,
+	}
+
+})
