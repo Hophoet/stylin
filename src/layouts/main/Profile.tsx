1daf056254f1aa38f4aa1a0e5faafeef1af3f5d3
hophoet <hophoet@gmail.com>
2021-07-03 02:18:27 +0000

    (layout) update profile component render user profile image render actions handle navigation the authentication screen

--- src/layouts/main/Profile.tsx
+++ src/layouts/main/Profile.tsx
@@ -6,84 +6,63 @@ import {
   View,
   Text,
   StatusBar,
+  Image,
   TouchableOpacity,
+  ImageBackground,
+  ScrollView,
+  FlatList
 } from "react-native";
-import LinearGradient from 'react-native-linear-gradient';
-import PhoneInput from "react-native-phone-number-input";
-import Svg, { Circle, Text as CsvText } from "react-native-svg";
-import { ApplicationProvider, Layout } from '@ui-kitten/components';
-
 import { bgLinearGradient, sideBarLinearGradient} from '../../assets/colors/main';
-import IconActionBox from '../../components/IconActionBox';
-import Icon from "react-native-vector-icons/Ionicons";
-import QRCodeModal from '../../components/modals/QRCodeModal';
+import CButton from '../../components/CButton';
+import ActionButton from '../../components/ActionButton';
+import {profileActionButtons} from '../../data/data'
 
-const iconActions = [
-	{icon:'wifi', action:()=>{}},
 
-]
+
 
 const Profile = ({navigation}:any) => {
 
-  const [value, setValue] = useState("");
   return (
       <View style={styles.container}>
-		<View style={styles.row1}>
-            	<Text style={styles.title}>Votre Profil</Text>
-            	<Text style={styles.description}>
-					Lorem aut, consectetur blanditiis enim  Ullam, ducimus excepturi!	Bienvenue
-				</Text>
-		</View>
-		<View style={styles.row2}>
-			<Svg width={width/2} height={width/2}>
-					<CsvText
-					// stroke="white"
-					fontSize="25"
-					x={width / 4}
-					y={width / 4}
-					fill='white'
-					textAnchor="middle"
-					>
-					12 525
-					</CsvText>
-					<CsvText
-					// stroke="white"
-					fontSize="17"
-					x={width / 4}
-					y={width / 3.2}
-					fill='white'
-					textAnchor="middle"
-					>
-						XOF
-					</CsvText>
-				 <Circle
-					cx={width/4}
-					cy={width/4}
-					r={width/5}
-					stroke={sideBarLinearGradient.top}
-					strokeWidth="2.5"
-					fill='none'
-				  />
-				{/* <Circle
-				  stroke="#2162cc"
-				  fill="none"
-				  cx={size / 2}
-				  cy={size / 2}
-				  r={radius}
-				  strokeDasharray={`${circunference} ${circunference}`}
-				/> */}
-			  </Svg>
-		</View>
-		<View style={styles.row3}>
-            	<Text style={styles.row3Title}>
-					Lorem ipsum  aut, consectetur lenz ldfk df
-					Lorem ipsum  aut, consectetur lené l
-				</Text>
-            	<Text style={styles.phoneNumber}>(228) 98 78 34 00</Text>
-				<QRCodeModal/>
-		</View>
-		<View style={styles.row4}>
-		</View>
+        <View style={styles.row1}>
+            <Text style={styles.headerTitle}>Profile</Text>
+        </View>
+        <View style={styles.row2}>
+          <View style={styles.row2Column1}>
+              <Image
+                style={styles.profileImage}
+                source={require('../../assets/images/bg1.jpg')}
+              />
+          </View>
+          <View style={styles.row2Column1}>
+            <Text style={styles.name}>Lina Hub</Text>  
+            <Text style={styles.email}>Lina@gmail.com</Text>  
+            <Text style={styles.username}>@linaHub</Text>  
+          </View>
+        </View>
+        <View style={styles.row3}>
+          <FlatList
+            showsVerticalScrollIndicator={false}
+            data={profileActionButtons}
+            keyExtractor={(item)=>item.id.toString()}
+            renderItem={({item, index})=>(
+              <ActionButton 
+                icon={'arrow-forward'}
+                item={item}/>
+            )}
+
+          />
+        </View>
+        <View style={styles.row4}>
+              <CButton 
+                  onPress={()=>{navigation.navigate('Auth')}}
+                  label='Log Out'
+                  iconPosition='left'
+                  iconColor='white'
+                  icon='log-out-outline' 
+                  textStyle={{color:'white'}}
+                  buttonStyle={{backgroundColor:'black'}}/>
+        </View>
       </View>
   );
 
@@ -96,45 +75,54 @@ const {width, height} = Dimensions.get('window');
 const styles = StyleSheet.create({
 	container:{
 		flex:1,
+    paddingHorizontal:20,
+    backgroundColor:'white',
 
 	},
-	contentContainer:{
-	},
-	title:{
-		color:sideBarLinearGradient.top,
-		fontSize:25,
-	},
-	description:{
-		paddingVertical:5,
-		paddingRight:20,
-		color:'white',
-		opacity:.5,
-	},
-	row1:{
+  row1:{
+    flex:1,
+  },
+  row2:{
+    flexDirection:'row',
+    // backgroundColor:'blue',
+    paddingVertical:20,
+  },
+  row2Column1:{
+    justifyContent:'center',
+    alignSelf:'center',
+    paddingRight:10,
+  },
+  profileImage:{
+      width:width/4,
+      height:width/4,
+      borderRadius:width/2,
 
-	},
-	row2:{
-		// backgroundColor:'red',
-		justifyContent:'center',
-		alignItems:'center',
-	},
-	row3:{
-		// paddingHorizontal:width/8,
-	},
-	row3Title:{
-		textAlign:'center',	
-		paddingVertical:5,
-		paddingRight:20,
-		color:'white',
-		opacity:.5,
-	},
-	row4Row:{
-		flexDirection:'row',
-		justifyContent:'center'
-	},
-	phoneNumber:{
-		color:sideBarLinearGradient.top,
-		fontSize:20,
-		alignSelf:'center',
-	},
+    },
+  name:{
+    fontSize:20,
+    fontWeight:'bold',
+  },
+  email:{
+    fontWeight:'bold',
+  },
+  username:{
+  },
+  row2Column2:{
+    justifyContent:'center',
+    alignSelf:'center'
+  },
+  headerTitle:{
+        paddingBottom:20,
+        fontSize:34,
+        fontWeight:'bold',
+    },
+    row3:{
+      paddingVertical:20,
+      flex:4,
+      // backgroundColor:'red',
+    },
+    row4:{
+      paddingVertical:20,
+      flex:1,
+    }
 })
