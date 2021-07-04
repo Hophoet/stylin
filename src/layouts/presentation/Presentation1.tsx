c7f01f3fb44e9e23cd7aa191566ef3bda96c7792
hophoet <hophoet@gmail.com>
2021-07-04 19:11:28 +0000

    (style) change component background to white

--- src/layouts/presentation/Presentation1.tsx
+++ src/layouts/presentation/Presentation1.tsx
@@ -47,6 +47,7 @@ const {width, height} = Dimensions.get('window');
 const styles = StyleSheet.create({
 	container:{
 		flex:1,
+		backgroundColor:'white',
 		//backgroundColor:'#6C63FF'
 	},
 	row1Title:{
