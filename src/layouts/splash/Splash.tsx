67b94194ae5ef81cbbc48e48a9c8aa8099a55065
hophoet <hophoet@gmail.com>
2021-07-04 19:04:14 +0000

    (layout) change splash screen component background to white

--- src/layouts/splash/Splash.tsx
+++ src/layouts/splash/Splash.tsx
@@ -27,6 +27,7 @@ const {width, height} = Dimensions.get('window');
 const styles = StyleSheet.create({
 	container:{
 		flex:1,
+		backgroundColor:'white',
 	},
 	contentContainer:{
 		// backgroundColor:'red',	
