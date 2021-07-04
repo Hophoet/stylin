aaa8ca7bddb4169ff98d41475e135b3ab171d34f
hophoet <hophoet@gmail.com>
2021-07-04 19:07:38 +0000

    (data) add orders test data

--- src/data/data.ts
+++ src/data/data.ts
@@ -24,10 +24,8 @@ const productSizes = [
 ]
 
 const profileActionButtons = [
-    { id:1, name:'My Order'},
-    { id:2, name:'My Cards'},
-    { id:3, name:'Payments'},
-    { id:5, name:'Change Email & Password'},
+    { id:1, name:'Update personal informations', code:'upi'},
+    { id:2, name:'My Orders', code:'mo'},
 ]
 
 
@@ -38,6 +36,13 @@ const products = [
     {id:4, name:'Black Shirt', caracteristics:'kjdf ldfjlsdfjsldfjs flsdfjlsfjsldfjskldfs dfslfjksldf sdfk', price:49, isFavorite:true, image:require('../assets/images/img5.jpg')},
 ]
 
+const orders = [
+    {id:2, price:900, quantity:4, date:(new Date()).toDateString()},
+    {id:3, price:300, quantity:12, date:(new Date(120008488)).toDateString()},
+    {id:4, price:30, quantity:3, date:(new Date(33)).toDateString()},
+    {id:5, price:9, quantity:2, date:(new Date('2020')).toDateString()},
+    {id:6, price:23, quantity:1, date:(new Date('2021')).toDateString()},
+]
 
 
 
@@ -46,5 +51,6 @@ export {
     products,
     productColors,
     productSizes,
-    profileActionButtons
+    profileActionButtons,
+    orders,
 }
